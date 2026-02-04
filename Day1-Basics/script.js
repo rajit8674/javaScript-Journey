060c69f74ce7a38a47755efcc5637fd5f259a49c
rajit8674 <rajitmaurya8318@gmail.com>
2026-02-05 00:04:21 +0530

    javascript-basics

---
+++ Day1-Basics/script.js
@@ -0,0 +1,48 @@
+// Day 1 JavaScript Practice
+
+console.log("JavaScript Journey - Day 1");
+
+// Variables
+const name = "Rajit";
+let age = 22;
+const city = "Delhi";
+
+// Data Types
+const isLearning = true;
+let score;
+const result = null;
+
+// Print values
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(isLearning);
+console.log(score);
+console.log(result);
+
+// Check types
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLearning);
+console.log(typeof score);
+console.log(typeof result);
+
+
+
+//Task 1
+const college = "ABC College";
+const course = "BCA";
+let year = 3;
+
+console.log(college, course, year);
+
+
+//Task 2
+let marks = 80;
+marks = 90;
+console.log(marks);
+
+//Task 3
+const pi = 3.14;
+// pi = 3.1415; // This will cause an error brecause pi is a constant
+console.log(pi);
