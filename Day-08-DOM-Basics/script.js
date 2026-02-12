664ba5630ccefdcf1f6f807fb8f5a0bbee378cbd
rajit8674 <rajitmaurya8318@gmail.com>
2026-02-12 23:21:05 +0530

    Day-8-DOM

--- Day-08-DOM-Basics/script.js
+++ Day-08-DOM-Basics/script.js
@@ -1,3 +1,16 @@
 // element select
 let heading = document.getElementById("title");
 let button = document.getElementById("btn");
+
+
+//text change krna 
+heading.textContent = "JavaScript DOM Started 🔥";
+
+//button click event
+button.addEventListener("click", function () {
+  heading.textContent = "Button Clicked 🚀";
+});
+
+
+//alternative method
+document.body.style.backgroundColor = "lightblue";
